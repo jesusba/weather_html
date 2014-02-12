0a29efb841e0cdef1ed9e89ce3e67b373090d936
jesusba <jesusba60@gmail.com>
2014-02-12 23:11:48 +0100

    Modificación del fichero PYTHON

--- weatherhtml.py
+++ weatherhtml.py
@@ -1,16 +1,45 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-from jinja2 import template
+import jinja2
+from jinja2 import Template
 
-ciudades = {"1":"Almería","2":"Cádiz","3":"Córdoba","4":"Granada","5":"Huelva","6":"Jaén","7":"Málaga","8":"Sevilla","9":"Dos Hermanas"}
+def direccionviento(direccion):
+	"""Función que calcula la dirección del
+	viento dada por grados"""
+	for grado in str(direccion):
+		if direccion >= 337.5 and direccion < 22.5:
+			return "N"
+		elif direccion >= 22.5 and direccion < 67.5:
+			return "NE"
+		elif direccion >= 67.5 and direccion < 112.5:
+			return "E"
+		elif direccion >= 112.5 and direccion < 157.5:
+			return "SE"
+		elif direccion >= 157.5 and direccion < 202.5:
+			return "S"
+		elif direccion >= 202.5 and direccion < 247.5:
+			return "SO"
+		elif direccion >= 247.5 and direccion < 292.5:
+			return "O"
+		elif direccion >= 292.5 and direccion < 337.5:
+			return "NO"
+		
+ciudades = ["Almería","Cádiz","Córdoba","Granada","Huelva","Jaén","Málaga","Sevilla","Dos Hermanas"]
 
-respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % ciudades[0})
+respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % ciudades[1]})
 
 dicc = json.loads(respuesta.text)
 
-temperatura = dicc["main"]["temp"]
+tempemin = dicc["main"]["temp_min"]
+tempemax = dicc["main"]["temp_max"]
+viento = dicc["wind"]["speed"]
+direccion = dicc["wind"]["deg"]
 
-tempreal = temperatura - 273
+tempminreal = round(tempemin - 273,1)
+tempmaxreal = round(tempemax - 273,1)
+vientoreal = round(viento * 1.61,1)
 
-print "La temperatura actual de %s es de %s grados centígrados" % (ciudades[peticion],tempreal)
+print "La temperatura mínima actual de %s es de %s grados centígrados, la máxima es de %s, y el viento tiene una velocidad de %s km/h dirección %s" % (ciudades[1],tempminreal,tempmaxreal,vientoreal,direccionviento(direccion))
+
+# print respuesta.text
